2da34b3a0d20f82f49d44c0ac78fe9046c198078
madinamantay <39566071+madinamantay@users.noreply.github.com>
2018-11-02 22:33:25 +0600

    Add files via upload

---
+++ lab7week11.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE lab7;
+
+CREATE INDEX countries_name ON countries(name);
+
+CREATE INDEX employees_name_surname ON employees(name,surname);
+
+create index salary on employees(salary);
+
+create index employees.substring on employees(substring(name from 1 for 4));
+
+create index department_id on employees(department_id,salary);
+
+create index budget on departments(department_id, budget);
